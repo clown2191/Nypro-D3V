d8ee390fb67fb7c3a1a1bf7ad595cbe76ac01b47
rayaviles <j.raymundo.aviles@gmail.com>
2016-10-04 17:16:16 -0700

    new project on going

---
+++ TempoMorte/TempoMorte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TempoMorte.Startup))]
+namespace TempoMorte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
